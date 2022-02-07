b0019bbff38d625c72e19c87a4ecf67973f05009
Samir <sam.djelouahi@gmail.com>
2022-02-07 15:05:21 +0100

    research bar

--- routes/films.js
+++ routes/films.js
@@ -1,37 +1,38 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const protectRoute = require("../middlewares/protectRoute");
-const FilmModel = require('./../models/Film.model');
+const FilmModel = require("./../models/Film.model");
 const uploader = require("../config/cloudinary");
 
 router.get("/films", (req, res, next) => {
-    FilmModel.find()
-      //.populate("author")
-      .then((dbFilms) => {
-        //console.log("all films found in the databases :", dbFilms);
-        res.status(201).json(dbFilms);
-      })
-      .catch((err) => {
-        console.error(
-          "there was an error while getting the films list from the database",
-          err)})
-        
+  
+  const q = !req.query.q ? {} : { name: req.query.q };
+
+  console.log(req.query);
+
+  FilmModel.find(q)
+    //.populate("author")
+    .then((dbFilms) => {
+      //console.log("all films found in the databases :", dbFilms);
+      res.status(201).json(dbFilms);
+    })
+    .catch((err) => {
+      console.error(
+        "there was an error while getting the films list from the database",
+        err
+      );
+    });
 });
 
 router.get("/films/:id", async (req, res, next) => {
-  try{
-
+  try {
     //console.log("jeusi la", req.params.id)
-    const oneFilm = await FilmModel.findById(req.params.id)
+    const oneFilm = await FilmModel.findById(req.params.id);
     //console.log("jeusi le film recherché", oneFilm)
     res.status(201).json(oneFilm);
-    
-    
-  }catch(err){
+  } catch (err) {
     next(err);
   }
-      
-  });
+});
 
-
-  module.exports = router;
+module.exports = router;
--- seeds/handle.js
+++ seeds/handle.js
@@ -1,37 +1,60 @@
 require("dotenv").config();
 require("./../config/dbConfig"); // fetch the db connection
-const FilmModel = require('./../models/Film.model.js');
-const axios = require('axios');
+const FilmModel = require("./../models/Film.model.js");
+const axios = require("axios");
 
-const filmsInfos = require('./simple.json');
-let counter = 0
-let intervalId = setInterval(() =>{
+const filmsInfos = require("./simple.json");
+
+let counter = 0;
+
+const finish = () => {
+ // clearInterval(intervalId);
+  console.log("Finished...");
+  process.exit();
+};
+
+let doRec = async  () => {
+  try {
     if (counter === filmsInfos.length - 1) {
-        clearInterval(intervalId);
-        console.log('Finished...')
-        process.exit()
+      finish();
     }
-    fetchApi(counter)
-    counter++
-}, 100)
+    await fetchApi(counter);
+    counter++;
+    setTimeout(doRec, 100);
+  } catch (err) {
+    console.log("err at interval", err);
+    finish();
+  }
+};
 
-async function fetchApi(counter) {
+ function fetchApi(counter) {
+  return new Promise(async (resolve, reject) => {
     try {
-        const response = await axios.get(`https://api.themoviedb.org/3/movie/${filmsInfos[counter].id}?api_key=f59dd0e5ecfa450f4c799f1c056451a6&language=en-US&append_to_response=credits,videos`)
-        const currentFilm = response.data
-        const newFilm = {
-            name: currentFilm.original_title,
-            releaseDate: currentFilm.release_date,
-            picture: `https://www.themoviedb.org/t/p/w1280${currentFilm.poster_path}`,
-            description: currentFilm.overview,
-            genre: currentFilm.genres[0]?.name || 'uncategorized',
-            crew: currentFilm.credits.crew,
-            cast: currentFilm.credits.cast,
-            videos: currentFilm.videos.results
-        }
-await FilmModel.create(newFilm)
-console.log(newFilm.name + ' inserted in the DB.');
-    } catch(e) {
-        console.log(e)
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${filmsInfos[counter].id}?api_key=f59dd0e5ecfa450f4c799f1c056451a6&language=en-US&append_to_response=credits,videos`
+      );
+
+      const currentFilm = response.data;
+
+      const newFilm = {
+        name: currentFilm.original_title,
+        releaseDate: currentFilm.release_date,
+        picture: `https://www.themoviedb.org/t/p/w1280${currentFilm.poster_path}`,
+        description: currentFilm.overview,
+        genre: currentFilm.genres[0]?.name || "uncategorized",
+        crew: currentFilm.credits.crew,
+        cast: currentFilm.credits.cast,
+        videos: currentFilm.videos.results,
+      };
+
+      const f = await FilmModel.create(newFilm);
+      const str = `${filmsInfos.length - counter} / ${filmsInfos.length}`;
+      console.log(str , " - " + newFilm.name + " inserted in the DB.");
+      resolve(f._id);
+    } catch (e) {
+      reject(e);
     }
+  });
 }
+
+doRec();
