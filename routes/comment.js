72d19223a6f97534e495173cd8b3f5466fdfa353
Patrick Hagège <hagege.patrick@gmail.com>
2022-02-08 10:24:20 +0100

    comment get phase 1

--- routes/comment.js
+++ routes/comment.js
@@ -6,16 +6,25 @@ const uploader = require("../config/cloudinary");
 const isAuthenticated = require('./../middlewares/jwt.middleware')
 
 
+router.get('/films/:id/comments', async (req, res, next) => {
+  try {
+    const comments = await CommentModel.find({ film: req.params.id })
+    res.status(201).json(comments)
+  } catch (error) {
+    next(error);
+  }
+})
 
-router.post("/:id/comments", isAuthenticated, async (req, res, next) => {
 
-    console.log('REQ BODY', req.body)
-    console.log('REQ PAYLOAD', req.payload)
+router.post("/films/:id/comments", isAuthenticated, async (req, res, next) => {
+
+    // console.log('REQ BODY', req.body)
+    // console.log('REQ PAYLOAD', req.payload)
 
     const newComment = {...req.body, film: req.params.id, author : req.payload._id}
 
     try {
-      console.log('TOTO' , req.body )
+      // console.log('TOTO' , req.body )
         const comments = await CommentModel.create(newComment);
         res.status(201).json(comments);
       } catch (er) {
