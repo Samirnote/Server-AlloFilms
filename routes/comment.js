bfbb5cdb3080c0153ac3d97c268e141a743dd724
Steven <stevenscemama@gmail.com>
2022-02-08 15:03:39 +0100

    patch done

--- routes/comment.js
+++ routes/comment.js
@@ -6,10 +6,12 @@ const uploader = require("../config/cloudinary");
 const isAuthenticated = require('./../middlewares/jwt.middleware')
 
 
+
 router.get('/films/:id/comments', async (req, res, next) => {
   try {
-    const comments = await CommentModel.find({ film: req.params.id })
-    res.status(201).json(comments)
+    const comments = await CommentModel.find({ film: req.params.id }).populate("author");
+    // const comments = await CommentModel.find({ film: req.params.id }).populate({path:"author"});
+    res.status(201).json(comments);
   } catch (error) {
     next(error);
   }
@@ -18,18 +20,36 @@ router.get('/films/:id/comments', async (req, res, next) => {
 
 router.post("/films/:id/comments", isAuthenticated, async (req, res, next) => {
 
-    // console.log('REQ BODY', req.body)
-    // console.log('REQ PAYLOAD', req.payload)
+  // console.log('REQ BODY', req.body)
+  // console.log('REQ PAYLOAD', req.payload)
 
-    const newComment = {...req.body, film: req.params.id, author : req.payload._id}
+  const newComment = { ...req.body, film: req.params.id, author: req.payload._id }
 
-    try {
-      // console.log('TOTO' , req.body )
-        const comments = await CommentModel.create(newComment);
-        res.status(201).json(comments);
-      } catch (er) {
-        console.error(er);
-      }
+  try {
+    // console.log('TOTO' , req.body )
+    const comments = await CommentModel.create(newComment);
+    res.status(201).json(comments);
+  } catch (er) {
+    console.error(er);
+  }
 });
+
+
+router.patch("/films/:id/comments/:idComment", isAuthenticated, async (req, res, next) => {
+  try {
+    const updatedComment = await CommentModel.findByIdAndUpdate(req.params.idComment, req.body, {new:true});
+    // console.log("req ", req);
+    console.log("req body ", req.body);
+    console.log("req.params.idComment ", req.params.idComment);
+    console.log("comment ", updatedComment);
+    res.status(201).json(updatedComment);
+  } catch (er) {
+    console.error(er)
+  }
+})
+
+// router.delete("/films/:id/comments/:idComment")
+
+
 
 module.exports = router;
